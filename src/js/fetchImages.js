5c2f91ce273491cd103e5b7f05b7c6046eed75bd
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-03-12 09:51:09 +0200

    fix bug

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -3,6 +3,7 @@ export const fetchImages = async (inputValue, pageNr) => {
     `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
     .then(async response => {
+
       if (!response.ok) {
         if (response.status === 404) {
           return [];
@@ -10,6 +11,7 @@ export const fetchImages = async (inputValue, pageNr) => {
         throw new Error(response.status);
       }
       return await response.json();
+     
     })
     .catch(error => {
       console.error(error);
--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,8 @@ btnLoadMore.style.display = 'none';
 
 let pageNumber = 1;
 
+
+
 btnSearch.addEventListener('click', e => {
   e.preventDefault();
   cleanGallery();
@@ -28,18 +30,25 @@ btnSearch.addEventListener('click', e => {
         Notiflix.Notify.success(
           `Hooray! We found ${foundData.totalHits} images.`
         );
-        btnLoadMore.style.display = 'block';
+
+        if (foundData.hits.length >= 40) {
+          btnLoadMore.style.display = 'block';
+        }
         gallerySimpleLightbox.refresh();
       }
     });
   }
 });
 
+
+
 btnLoadMore.addEventListener('click', () => {
   pageNumber++;
   const trimmedValue = input.value.trim();
   btnLoadMore.style.display = 'none';
   fetchImages(trimmedValue, pageNumber).then(foundData => {
+   
+    
     if (foundData.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -49,13 +58,16 @@ btnLoadMore.addEventListener('click', () => {
       Notiflix.Notify.success(
         `Hooray! We found ${foundData.totalHits} images.`
       );
-      btnLoadMore.style.display = 'block';
+      if (foundData.hits.length >= 40) {
+        btnLoadMore.style.display = 'block';
+      }
+      
     }
   });
 });
 
+
 function renderImageList(images) {
-  console.log(images, 'images');
   const markup = images
     .map(image => {
       console.log('img', image);
@@ -82,6 +94,8 @@ function renderImageList(images) {
     .join('');
   gallery.innerHTML += markup;
 }
+
+
 
 function cleanGallery() {
   gallery.innerHTML = '';
